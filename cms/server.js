9361ae95135ffd3145b3f2592fde970f0e32e842
TurkeyAbdo <wizardman0008@gmail.com>
2026-06-15 14:54:09 +0200

    Add custom server.js entry for Hostinger Express preset
    Hostinger's Node.js Express framework preset requires an entry
    file (server.js) that starts a persistent process. Strapi doesn't
    ship one by default; this loads the compiled build and starts it.

---
+++ cms/server.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const { createStrapi, compileStrapi } = require('@strapi/strapi');
+
+async function main() {
+  const appContext = await compileStrapi();
+  const app = await createStrapi(appContext).load();
+  await app.start();
+}
+
+main();
